5284878fe56778e0adde93728701c48e05473a45
Dominique Martinet <asmadeus@codewreck.org>
2018-09-16 01:40:13 +0900

    tests: add options param to create_screen()
    Always apply defaults when creating a screen (the C-side global state
    default will not have the intended values without this), and provide a
    way to override default values with a new optional parameter

--- kitty_tests/history.py
+++ kitty_tests/history.py
@@ -3,14 +3,12 @@
 # License: GPL v3
 
 from . import BaseTest
-from kitty.fast_data_types import DECAWM, IRM, Cursor, DECCOLM, DECOM
-
 
 
 class TestHistory(BaseTest):
 
     def test_history_short(self):
-        s = self.create_screen()
+        s = self.create_screen(options={'scrollback_pager_history_size': 10})
 
         for i in range(1, 1000*1000):
             s.draw('aaa')
@@ -18,9 +16,9 @@ class TestHistory(BaseTest):
             s.carriage_return()
 
     def test_history_long(self):
-        s = self.create_screen()
+        s = self.create_screen(options={'scrollback_pager_history_size': 10})
 
         for i in range(1, 1000*200):
-            s.draw('a' * 5 * 50)
+            s.draw('aaaaa' * 50)
             s.linefeed()
             s.carriage_return()
